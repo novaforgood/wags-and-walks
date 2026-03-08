65ed0230bf8e7f1ce339ba01d4b58c1a1089fe81
Jay-Thpr <ja.thapar@gmail.com>
2026-03-08 14:40:26 -0700

    deleted deprecated files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import Navigation from './components/Navigation'
 import { PeopleProvider } from './components/PeopleProvider'
 
 export const metadata: Metadata = {
@@ -22,7 +21,6 @@ export default function RootLayout({
       </head>
       <body>
         <PeopleProvider>
-          <Navigation />
           {children}
         </PeopleProvider>
       </body>
